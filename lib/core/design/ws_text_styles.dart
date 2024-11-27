b7538c97c002173fc93dbac278eaa75acda47a4d
Vinicius Ornelas <ornelas95@hotmail.com>
2024-11-26 22:25:10 -0300

    generate a text style design

---
+++ lib/core/design/ws_text_styles.dart
@@ -0,0 +1,63 @@
+import 'dart:ui';
+
+class WsTextStyles {
+//generate a text style design for application
+  static TextStyle h1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle h2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle h3 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle h4 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle h5 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle h6 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle subtitle1 = TextStyle(
+    fontSize: 16,
+  );
+
+  static TextStyle subtitle2 = TextStyle(
+    fontSize: 14,
+  );
+
+  static TextStyle body1 = TextStyle(
+    fontSize: 16,
+  );
+
+  static TextStyle body2 = TextStyle(
+    fontSize: 14,
+  );
+
+  static TextStyle button = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle caption = TextStyle(
+    fontSize: 12,
+  );
+
+  static TextStyle overline = TextStyle(
+    fontSize: 10,
+  );
+}
