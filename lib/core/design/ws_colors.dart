4fc58c218b3c0f93f9c9c68a747baa43b82c09bb
Vinicius Ornelas <ornelas95@hotmail.com>
2024-11-26 22:20:50 -0300

    create initial color theme

---
+++ lib/core/design/ws_colors.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+class WsColors {
+  // genearate colors for the project
+  static const Color primary = Color(0xFF3F51B5);
+  static const Color primaryDark = Color(0xFF303F9F);
+  static const Color primaryLight = Color(0xFFC5CAE9);
+
+  static const Color secondary = Color(0xFF4CAF50);
+  static const Color secondaryDark = Color(0xFF388E3C);
+  static const Color secondaryLight = Color(0xFFC8E6C9);
+
+  static const Color background = Color(0xFFE0E0E0);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color error = Color(0xFFB00020);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color onSecondary = Color(0xFF000000);
+  static const Color onBackground = Color(0xFF000000);
+  static const Color onSurface = Color(0xFF000000);
+
+  static const Color success = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFFC107);
+  static const Color info = Color(0xFF2196F3);
+  static const Color danger = Color(0xFFD32F2F);
+  static const Color light = Color(0xFFE0E0E0);
+  static const Color dark = Color(0xFF212121);
+}
