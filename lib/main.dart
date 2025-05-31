59c00fd0067f0f6a4032e90d081e33b7248a893a
OrnelasD-Rogers <ornelas95@hotmail.com>
2025-05-31 13:02:23 -0300

    add snackbar Utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_workshop_front/app_route.dart';
+import 'package:flutter_workshop_front/utils/snackbar_util.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,15 +10,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      scaffoldMessengerKey: SnackBarUtil().scaffoldKey,
       routerConfig: router,
       localizationsDelegates: GlobalMaterialLocalizations.delegates,
       locale: const Locale('pt', 'BR'),
       supportedLocales: const [
-        Locale('pt', 'BR'), // Adiciona PT-BR
+        Locale('pt', 'BR'),
       ],
     );
   }
---
+++ lib/utils/snackbar_util.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class SnackBarUtil {
+  static final SnackBarUtil _instance = SnackBarUtil._internal();
+  factory SnackBarUtil() => _instance;
+  SnackBarUtil._internal();
+
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  GlobalKey<ScaffoldMessengerState> get scaffoldKey => _scaffoldKey;
+
+  void showSuccess(String message) {
+    _showSnackBar(
+      message,
+      backgroundColor: Colors.green,
+      textColor: Colors.white,
+    );
+  }
+
+  void showError(String message) {
+    _showSnackBar(
+      message,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+    );
+  }
+
+  void showInfo(String message) {
+    _showSnackBar(
+      message,
+      backgroundColor: Colors.blue,
+      textColor: Colors.white,
+    );
+  }
+
+  void showWarning(String message) {
+    _showSnackBar(
+      message,
+      backgroundColor: Colors.orange,
+      textColor: Colors.white,
+    );
+  }
+
+  void _showSnackBar(
+    String message, {
+    required Color backgroundColor,
+    required Color textColor,
+  }) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: textColor),
+      ),
+      backgroundColor: backgroundColor,
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+        label: 'OK',
+        textColor: Colors.white,
+        onPressed: () {
+          _scaffoldKey.currentState?.hideCurrentSnackBar();
+        },
+      ),
+    );
+
+    _scaffoldKey.currentState?.showSnackBar(snackBar);
+  }
+}
