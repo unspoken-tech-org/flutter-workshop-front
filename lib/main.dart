babddee5482b496892d177afb9bdc04f86eabfe8
OrnelasD-Rogers <ornelas95@hotmail.com>
2025-07-11 09:58:58 -0300

    Disable debug banner in MaterialApp for a cleaner UI presentation

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       scaffoldMessengerKey: SnackBarUtil().scaffoldKey,
       routerConfig: router,
       localizationsDelegates: GlobalMaterialLocalizations.delegates,
