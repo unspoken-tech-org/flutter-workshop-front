7a6230da3a0a56ed5e55e1d0f6bb037e7d602a84
OrnelasD-Rogers <ornelas95@hotmail.com>
2025-12-02 15:09:31 -0300

    Add DuplicateRequestInterceptor to prevent duplicate HTTP requests and integrate it into CustomDio

--- lib/core/http/custom_dio.dart
+++ lib/core/http/custom_dio.dart
@@ -1,13 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_workshop_front/core/config/config.dart';
+import 'package:flutter_workshop_front/core/http/interceptors/duplicate_request_interceptor.dart';
 
 class CustomDio {
-  static Dio dioInstance() => Dio(
-        BaseOptions(
-          baseUrl: Config.baseUrl,
-          connectTimeout: const Duration(seconds: 15),
-          receiveTimeout: const Duration(seconds: 15),
-          validateStatus: (status) => status! < 500,
-        ),
-      );
+  static Dio dioInstance() {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: Config.baseUrl,
+        connectTimeout: const Duration(seconds: 15),
+        receiveTimeout: const Duration(seconds: 15),
+        validateStatus: (status) => status! < 500,
+      ),
+    );
+
+    dio.interceptors.add(DuplicateRequestInterceptor());
+
+    return dio;
+  }
 }
