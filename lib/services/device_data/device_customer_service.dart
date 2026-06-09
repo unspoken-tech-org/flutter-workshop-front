84363a43d5d9d27eb7270d2de87ff75bcfeea9a4
Vinicius Ornelas <ornelas95@hotmail.com>
2026-06-09 18:46:52 -0300

    refactor: split status/urgency/revision into dedicated endpoints

--- lib/services/device_data/device_customer_service.dart
+++ lib/services/device_data/device_customer_service.dart
@@ -6,6 +6,7 @@ import 'package:flutter_workshop_front/models/customer_device/device_customer.da
 import 'package:flutter_workshop_front/models/device/device_filter.dart';
 import 'package:flutter_workshop_front/models/device/device_input.dart';
 import 'package:flutter_workshop_front/models/home_table/device_data_table.dart';
+import 'package:flutter_workshop_front/models/home_table/status_enum.dart';
 import 'package:flutter_workshop_front/models/pageable/page_model.dart';
 
 class DeviceCustomerService {
@@ -39,6 +40,48 @@ class DeviceCustomerService {
     var json = deviceCustomer.toJson();
 
     Response response = await dio.put('/v1/device/update', data: json);
+
+    if (response.statusCode != 200) {
+      throw RequisitionException.fromJson(response.data['error']);
+    }
+
+    return DeviceCustomer.fromJson(response.data);
+  }
+
+  Future<DeviceCustomer> updateDeviceStatus(
+      int deviceId, StatusEnum status) async {
+    Response response = await dio.put(
+      '/v1/device/update/$deviceId/status',
+      data: {'deviceStatus': status.dbName},
+    );
+
+    if (response.statusCode != 200) {
+      throw RequisitionException.fromJson(response.data['error']);
+    }
+
+    return DeviceCustomer.fromJson(response.data);
+  }
+
+  Future<DeviceCustomer> updateDeviceHasUrgency(
+      int deviceId, bool hasUrgency) async {
+    Response response = await dio.put(
+      '/v1/device/update/$deviceId/urgency',
+      data: {'hasUrgency': hasUrgency},
+    );
+
+    if (response.statusCode != 200) {
+      throw RequisitionException.fromJson(response.data['error']);
+    }
+
+    return DeviceCustomer.fromJson(response.data);
+  }
+
+  Future<DeviceCustomer> updateDeviceRevision(
+      int deviceId, bool revision) async {
+    Response response = await dio.put(
+      '/v1/device/update/$deviceId/revision',
+      data: {'revision': revision},
+    );
 
     if (response.statusCode != 200) {
       throw RequisitionException.fromJson(response.data['error']);
