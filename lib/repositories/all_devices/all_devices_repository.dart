8bc68dcb31dbc38bedc75c120747113e80ddacac
Vinicius Ornelas <ornelas95@hotmail.com>
2026-06-10 16:40:27 -0300

    Add DeviceSearchFilter model and searchDevices endpoint integration

--- lib/repositories/all_devices/all_devices_repository.dart
+++ lib/repositories/all_devices/all_devices_repository.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_workshop_front/models/device/device_filter.dart';
+import 'package:flutter_workshop_front/models/device/device_search_filter.dart';
 import 'package:flutter_workshop_front/models/device_brand/device_brand_model.dart';
 import 'package:flutter_workshop_front/models/device_type.dart/device_type_model.dart';
 import 'package:flutter_workshop_front/models/home_table/device_data_table.dart';
@@ -6,6 +7,8 @@ import 'package:flutter_workshop_front/models/pageable/page_model.dart';
 
 abstract class AllDevicesRepository {
   Future<Page<DeviceDataTable>> getAllDevicesFiltering([DeviceFilter? filter]);
+  Future<Page<DeviceDataTable>> searchDevices(
+      [DeviceSearchFilter? filter]);
   Future<List<DeviceType>> getAllDeviceTypes([String? name]);
   Future<List<DeviceBrand>> getAllDeviceBrands([String? name]);
 }
