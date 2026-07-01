c5abc62015bbc2fb3cb94d1aa77fb3962a409098
Vinicius Ornelas <ornelas95@hotmail.com>
2026-07-01 16:50:00 -0300

    Add configurable debounce to WsSearchBarWidget
    - Add debounceTime parameter (default 300ms) to WsSearchBarWidget
    - Wrap onChanged callback with Debouncer to prevent rapid search calls
    - Cancel debounce timer on clear button press and widget dispose
    - Remove duplicate debounce from AllCustomerController (was 500ms)
    - Fixes device search bug where typing 4+ digits skipped results

--- lib/widgets/ws_search_bar_widget.dart
+++ lib/widgets/ws_search_bar_widget.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workshop_front/core/debouncer.dart';
 import 'package:flutter_workshop_front/core/design/ws_text_styles.dart';
 
 class WsSearchBarWidget extends StatefulWidget {
   final String labelText;
   final void Function(String)? onChanged;
   final void Function()? onClear;
+  final int debounceTime;
 
   const WsSearchBarWidget({
     super.key,
     required this.labelText,
     required this.onChanged,
     required this.onClear,
+    this.debounceTime = 300,
   });
 
   @override
@@ -19,13 +22,24 @@ class WsSearchBarWidget extends StatefulWidget {
 
 class _WsSearchBarWidgetState extends State<WsSearchBarWidget> {
   final TextEditingController _searchController = TextEditingController();
+  late final Debouncer _debouncer =
+      Debouncer(milliseconds: widget.debounceTime);
+
+  @override
+  void dispose() {
+    _debouncer.cancel();
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: _searchController,
       onChanged: (value) {
-        widget.onChanged?.call(value);
+        _debouncer.run(() {
+          widget.onChanged?.call(value);
+        });
         setState(() {});
       },
       decoration: InputDecoration(
@@ -53,6 +67,7 @@ class _WsSearchBarWidgetState extends State<WsSearchBarWidget> {
             ? IconButton(
                 icon: const Icon(Icons.clear),
                 onPressed: () {
+                  _debouncer.cancel();
                   _searchController.clear();
                   widget.onClear?.call();
                 },
